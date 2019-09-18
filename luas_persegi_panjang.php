7cbc3489ed840782a1b851f43a5df3bc71c21301
zirifqi <azizimohrifqi@gmail.com>
2019-09-18 09:07:57 +0700

    Versi 0.1

---
+++ luas_persegi_panjang.php
@@ -0,0 +1,15 @@
+
+<?php
+/**
+  * Fungsi untuk menghitung luas persegi panjang
+  * @param int $panjang variable input panjang persegi panjang 
+  * @param int $lebar variable input lebar persegi panjang 
+  * @param int $luas perhitungan luas, yaitu $panjang * $lebar
+  * @author Moh. Rifqi Azizi
+  */
+
+  function luasPersegiPanjang($panjang, $lebar){
+    $luas = $panjang * $lebar;
+    return $luas;
+  }
+?> 
